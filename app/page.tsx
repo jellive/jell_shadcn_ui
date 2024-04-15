eea87e526b13bb4108e33bbc0cca8f8515cbb5e7
PD Jell <jellive7@gmail.com>
2024-04-15 17:13:06 +0900

    [setup] add basePath

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import config from 'next/config'
+
+const {basePath}: {basePath: string} = config().publicRuntimeConfig
+console.log('basePath', basePath)
 
 export default function Home() {
   return (
@@ -16,8 +20,17 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <Image
+            {/* <Image
               src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            /> */}
+
+<Image
+              src={basePath + '/vercel.svg'}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -29,9 +42,17 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
+        {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        /> */}
+                <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src={basePath + '/next.svg'}
           alt="Next.js Logo"
           width={180}
           height={37}
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/absproxy/3000'
+
+const publicRuntimeConfig = {
+    // basePath: process.env.NODE_ENV === 'development' ? `/absproxy/${'3000'}` : '/',
+    basePath: `/absproxy/${'3000'}`
 };
 
-export default nextConfig;
+const serverRuntimeConfig = {}
+
+
+const nextConfig = {
+    basePath: process.env.NODE_ENV === 'development' ? `/absproxy/${'3000'}` : '/',
+    publicRuntimeConfig,
+    serverRuntimeConfig
+}
+export default nextConfig
+// export default {publicRuntimeConfig, serverRuntimeConfig};
